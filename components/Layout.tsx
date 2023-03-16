3a0c9259df1f4ad74574fb87c6bce0e3fed6a077
notray.hackshaw@gmail.com <36725950+Ray-Hackshaw@users.noreply.github.com>
2023-03-16 22:32:50 +1300

    refactor: moving header to separate component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,65 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-
-interface INavItem {
-  name: string;
-  href: string;
-}
-
-const navigationItems: INavItem[] = [
-  {
-    name: "Products",
-    href: "/products",
-  },
-
-  {
-    name: "Archive",
-    href: "/archive",
-  },
-  {
-    name: "Gallery",
-    href: "/gallery",
-  },
-  {
-    name: "About",
-    href: "/about",
-  },
-  {
-    name: "Contact",
-    href: "/contact",
-  },
-];
+import { Header } from "./Header";
 
 export const Layout = ({ children }: { children: any }) => {
   return (
     <>
-      <Head>
-        <title>Velvet Terrain</title>
-      </Head>
-      <div className="bg-[#5a8163] sticky top-0 flex p-4 text-[#f0e4cc] items-center">
-        <div>
-          <Image
-            src="/images/velvet-terrain.png"
-            alt="shouldnt have smoked that shit"
-            width={100}
-            height={100}
-          />
-        </div>
-        <div className="text-center text-2xl px-4 flex flex-col justify-center items-center">
-          <p>Velvet</p>
-          <p>Terrain</p>
-        </div>
-        <ul className="flex gap-4 p-4">
-          {navigationItems.map((item: INavItem) => (
-            <Link key={item.href} href={item.href}>
-              <p className="hover:underline hover:transition hover:duration-100 hover:ease-in-out">
-                {item.name}
-              </p>
-            </Link>
-          ))}
-        </ul>
-      </div>
+      <Header />
       <main className="w-full primary-background min-h-screen mx-auto px-4">
         {children}
       </main>
